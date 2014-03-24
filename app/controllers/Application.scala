e1dd43cb67aa7d5c1129d9cf2b7eb7bcfffcabca
Dirceu Semighini Filho <dirceu.semighini@gmail.com>
2014-03-24 14:27:05 -0300

    Css e controller com conexão ao webwservice do tweeter

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,6 +8,12 @@ import play.api.libs.concurrent._
 import scala.concurrent.duration._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.EventSource
+import twitter4j._
+import twitter4j.{Status => TwStatus}
+import scala.Some
+import play.api.libs.ws.WS
+import play.api.libs.oauth.{RequestToken, ConsumerKey, OAuthCalculator}
+import play.libs.Json
 
 object Application extends Controller {
 
@@ -17,6 +23,10 @@ object Application extends Controller {
    */
   lazy val clock: Enumerator[String] = {
 
+    //    val twitter = TwitterFactory.getSingleton();
+
+    val twitterStream = new TwitterStreamFactory().getInstance()
+
     import java.util._
     import java.text._
 
@@ -24,10 +34,34 @@ object Application extends Controller {
 
     val dateFormat = new SimpleDateFormat("HH mm ss")
 
+
     Enumerator.generateM {
       Promise.timeout(Some(dateFormat.format(new Date)), 100 milliseconds)
     }
   }
+
+  def stream(keywords: String) = WebSocket.using[String] {
+    request =>
+
+      val out: Enumerator[String] = Concurrent.unicast[String](onStart = pushee => {
+        val consumerKey = ConsumerKey("", "")
+        val accessToken = RequestToken("", "")
+
+        def twitterIteratee = Iteratee.foreach[Array[Byte]] {
+          ba =>
+            val msg = new String(ba, "UTF-8")
+            pushee.push(msg)
+            println(msg)
+        }
+
+        WS.url("https://stream.twitter.com/1.1/statuses/filter.json?track=" + keywords)
+          .sign(OAuthCalculator(consumerKey, accessToken))
+          .get(headers => twitterIteratee)
+      })
+      val in = Iteratee.ignore[String]
+      (in, out)
+  }
+
 
   def index = Action {
     Ok(views.html.index())
@@ -37,5 +71,33 @@ object Application extends Controller {
     Ok.chunked(clock &> EventSource()).as("text/event-stream")
   }
 
+
+
+
+  def tweets(query:String): Enumerator[String] = {
+    Concurrent.unicast[String](onStart = pushee => {
+      val consumerKey = ConsumerKey("aiuO34OBqnrNhpAYzRlQ", "lUkhaS3We3HAH8pGtldC7fNEHnfZU6eWiWgTWKZ8ObQ")
+      val accessToken = RequestToken("86628543-35lewf8PWmE2QTpHvYhpjBITTbMUZZfQSDRGzfgss", "evnf4cDO69YnMfqEqvT7bKld9RjWJUBPCevh9bXx6O3L0")
+
+      def twitterIteratee = Iteratee.foreach[Array[Byte]] {
+        ba =>
+          val msg = new String(ba, "UTF-8")
+          val json = Json.parse(msg)
+          pushee.push(msg.replaceAll("'",""))
+          println(msg)
+      }
+
+      WS.url("https://stream.twitter.com/1.1/statuses/filter.json?track=" + query)
+        .sign(OAuthCalculator(consumerKey, accessToken))
+        .get(headers => twitterIteratee)
+    })
+  }
+
+  def liveTweet = Action {
+    val keywords = ""
+
+
+    Ok.chunked(tweets("iphone") &> EventSource()).as("text/event-stream")
+  }
 
 }
--- app/controllers/WebSocketController.scala
+++ app/controllers/WebSocketController.scala
@@ -1,8 +1,17 @@
 package controllers
 
+import play.api._
+import twitter4j.{QueryResult, Query, TwitterFactory}
+import play.mvc.Controller
+
 /**
  * Created by dirceu on 3/24/14.
  */
-class WebSocketController {
+class WebSocketController extends Controller {
+  //The factory instance is re-useable and thread safe.
+    val twitter = TwitterFactory.getSingleton();
+    val query = new Query("source:twitter4j yusukey");
+    val result = twitter.search(query);
+
 
 }
